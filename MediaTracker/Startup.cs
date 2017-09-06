2524ee07afdd21e5a0626ea7b491d15e38ce76c7
Kevin Mason <kevin.mason@outlook.com>
2017-09-06 15:23:12 -0500

    Add project files.

---
+++ MediaTracker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MediaTracker.Startup))]
+namespace MediaTracker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
