a02ec5b882c6e0ef8208116b6f9afd96e24bfe8c
Kevin Mason <kevin.mason@outlook.com>
2017-09-11 14:23:43 -0500

    MediaTracker - Form Creation; ViewModel Creation

---
+++ MediaTracker/ViewModels/MediaFormViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MediaTracker.ViewModels
+{
+    public class MediaFormViewModel
+    {
+        public string  Viewer { get; set; }
+        public string Date { get; set; }
+        public string Type { get; set; }
+        public string Title { get; set; }
+
+    }
+}
